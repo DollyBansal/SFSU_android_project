dd8b02cd4d3187c56991d49364f8058932d110ac
Dolly Bansal <dolly.bansal11@gmail.com>
2014-10-08 18:15:54 -0700

    Adding UI images

--- src/com/memoir/MainActivity.java
+++ src/com/memoir/MainActivity.java
@@ -1,5 +1,6 @@
 package com.memoir;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ActionBar actionBar = getActionBar();
+		actionBar.show();
+		
 	}
 
 	@Override
